9f986d5ec38f4b1f6804d5e769978f1cbb74f930
Ryan Hutchinson <ryanahutchinson@gmail.com>
2015-12-23 00:18:18 -0600

    Initial Commit. All functions working properly.

--- COEJOBFAIR_Alpha/COEJOBFAIR_Alpha/Main.Designer.cs
+++ COEJOBFAIR_Alpha/COEJOBFAIR_Alpha/Main.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace COEJOBFAIR_Alpha
+﻿using System;
+
+namespace COEJOBFAIR_Alpha
 {
     partial class Main
     {
@@ -28,41 +30,29 @@
         /// </summary>
         public void InitializeComponent()
         {
-            this.lbl_test = new System.Windows.Forms.Label();
-            this.lbl_test2 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.txt_id = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.lbl_test6 = new System.Windows.Forms.Label();
-            this.lbl_test5 = new System.Windows.Forms.Label();
-            this.lbl_test4 = new System.Windows.Forms.Label();
-            this.lbl_test3 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.debugToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.debug_cnn_chck = new System.Windows.Forms.ToolStripMenuItem();
+            this.databaseCheckToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.status_cnn = new System.Windows.Forms.Button();
+            this.status_db = new System.Windows.Forms.Button();
+            this.pBar1 = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // lbl_test
-            // 
-            this.lbl_test.AutoSize = true;
-            this.lbl_test.Location = new System.Drawing.Point(831, 179);
-            this.lbl_test.Name = "lbl_test";
-            this.lbl_test.Size = new System.Drawing.Size(57, 13);
-            this.lbl_test.TabIndex = 1;
-            this.lbl_test.Text = "Test Label";
-            // 
-            // lbl_test2
-            // 
-            this.lbl_test2.AutoSize = true;
-            this.lbl_test2.Location = new System.Drawing.Point(834, 206);
-            this.lbl_test2.Name = "lbl_test2";
-            this.lbl_test2.Size = new System.Drawing.Size(57, 13);
-            this.lbl_test2.TabIndex = 2;
-            this.lbl_test2.Text = "Test Label";
             // 
             // txt_id
             // 
-            this.txt_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_id.Location = new System.Drawing.Point(576, 390);
+            this.txt_id.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_id.Location = new System.Drawing.Point(449, 192);
             this.txt_id.Multiline = true;
             this.txt_id.Name = "txt_id";
-            this.txt_id.Size = new System.Drawing.Size(239, 62);
+            this.txt_id.Size = new System.Drawing.Size(453, 117);
             this.txt_id.TabIndex = 0;
             this.txt_id.Text = "R1257448";
             this.txt_id.TextChanged += new System.EventHandler(this.txt_id_TextChanged);
@@ -71,71 +61,133 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // lbl_test6
+            // statusStrip1
             // 
-            this.lbl_test6.AutoSize = true;
-            this.lbl_test6.Location = new System.Drawing.Point(831, 138);
-            this.lbl_test6.Name = "lbl_test6";
-            this.lbl_test6.Size = new System.Drawing.Size(57, 13);
-            this.lbl_test6.TabIndex = 5;
-            this.lbl_test6.Text = "Test Label";
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 521);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1033, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
             // 
-            // lbl_test5
+            // toolStripDropDownButton1
             // 
-            this.lbl_test5.AutoSize = true;
-            this.lbl_test5.Location = new System.Drawing.Point(831, 109);
-            this.lbl_test5.Name = "lbl_test5";
-            this.lbl_test5.Size = new System.Drawing.Size(57, 13);
-            this.lbl_test5.TabIndex = 6;
-            this.lbl_test5.Text = "Test Label";
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.debugToolStripMenuItem});
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(13, 20);
+            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
             // 
-            // lbl_test4
+            // debugToolStripMenuItem
             // 
-            this.lbl_test4.AutoSize = true;
-            this.lbl_test4.Location = new System.Drawing.Point(831, 81);
-            this.lbl_test4.Name = "lbl_test4";
-            this.lbl_test4.Size = new System.Drawing.Size(57, 13);
-            this.lbl_test4.TabIndex = 7;
-            this.lbl_test4.Text = "Test Label";
+            this.debugToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.debug_cnn_chck,
+            this.databaseCheckToolStripMenuItem,
+            this.resetToolStripMenuItem});
+            this.debugToolStripMenuItem.Name = "debugToolStripMenuItem";
+            this.debugToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.debugToolStripMenuItem.Text = "Debug";
             // 
-            // lbl_test3
+            // debug_cnn_chck
             // 
-            this.lbl_test3.AutoSize = true;
-            this.lbl_test3.Location = new System.Drawing.Point(831, 59);
-            this.lbl_test3.Name = "lbl_test3";
-            this.lbl_test3.Size = new System.Drawing.Size(57, 13);
-            this.lbl_test3.TabIndex = 8;
-            this.lbl_test3.Text = "Test Label";
+            this.debug_cnn_chck.Name = "debug_cnn_chck";
+            this.debug_cnn_chck.Size = new System.Drawing.Size(172, 22);
+            this.debug_cnn_chck.Text = "Connection Check";
+            this.debug_cnn_chck.Click += new System.EventHandler(this.debug_connection);
+            // 
+            // databaseCheckToolStripMenuItem
+            // 
+            this.databaseCheckToolStripMenuItem.Name = "databaseCheckToolStripMenuItem";
+            this.databaseCheckToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.databaseCheckToolStripMenuItem.Text = "Database Check";
+            this.databaseCheckToolStripMenuItem.Click += new System.EventHandler(this.debug_database);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // status_cnn
+            // 
+            this.status_cnn.BackColor = System.Drawing.Color.DarkGreen;
+            this.status_cnn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.status_cnn.Enabled = false;
+            this.status_cnn.ForeColor = System.Drawing.Color.Cornsilk;
+            this.status_cnn.Location = new System.Drawing.Point(0, 498);
+            this.status_cnn.Name = "status_cnn";
+            this.status_cnn.Size = new System.Drawing.Size(1033, 23);
+            this.status_cnn.TabIndex = 10;
+            this.status_cnn.Text = "Connection";
+            this.status_cnn.UseVisualStyleBackColor = false;
+            this.status_cnn.Visible = false;
+            // 
+            // status_db
+            // 
+            this.status_db.BackColor = System.Drawing.Color.DarkGreen;
+            this.status_db.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.status_db.ForeColor = System.Drawing.Color.Cornsilk;
+            this.status_db.Location = new System.Drawing.Point(0, 475);
+            this.status_db.Name = "status_db";
+            this.status_db.Size = new System.Drawing.Size(1033, 23);
+            this.status_db.TabIndex = 12;
+            this.status_db.Text = "Database";
+            this.status_db.UseVisualStyleBackColor = false;
+            this.status_db.Visible = false;
+            // 
+            // pBar1
+            // 
+            this.pBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pBar1.Location = new System.Drawing.Point(0, 449);
+            this.pBar1.Name = "pBar1";
+            this.pBar1.Size = new System.Drawing.Size(1033, 26);
+            this.pBar1.TabIndex = 13;
+            this.pBar1.Visible = false;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1033, 543);
-            this.Controls.Add(this.lbl_test3);
-            this.Controls.Add(this.lbl_test4);
-            this.Controls.Add(this.lbl_test5);
-            this.Controls.Add(this.lbl_test6);
+            this.Controls.Add(this.pBar1);
+            this.Controls.Add(this.status_db);
+            this.Controls.Add(this.status_cnn);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.txt_id);
-            this.Controls.Add(this.lbl_test2);
-            this.Controls.Add(this.lbl_test);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Main";
             this.Text = "Main";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
-      
+
+        
+
 
         #endregion
-        public System.Windows.Forms.Label lbl_test;
-        public System.Windows.Forms.Label lbl_test2;
         public System.Windows.Forms.TextBox txt_id;
         public System.Windows.Forms.OpenFileDialog openFileDialog1;
-        public System.Windows.Forms.Label lbl_test6;
-        public System.Windows.Forms.Label lbl_test5;
-        public System.Windows.Forms.Label lbl_test4;
-        public System.Windows.Forms.Label lbl_test3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem debugToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem debug_cnn_chck;
+        public System.Windows.Forms.Button status_cnn;
+        public System.Windows.Forms.Button status_db;
+        public System.Windows.Forms.ProgressBar pBar1;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem databaseCheckToolStripMenuItem;
     }
 }
 
