d0dda1801ad0404e78ef994c85da7c7c5c86a2fd
Ryan Hutchinson <ryanahutchinson@gmail.com>
2016-01-16 23:51:38 -0600

    Only Includes the database mainpulation nothing else.  NOT FOR INPUT BRANCH AND NOT WORKING!! NOT TESTED

--- COEJOBFAIR_Alpha/COEJOBFAIR_Alpha/DBcls.cs
+++ COEJOBFAIR_Alpha/COEJOBFAIR_Alpha/DBcls.cs
@@ -7,11 +7,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace COEJOBFAIR_Alpha
 {
     class Data_cnct : Main
     {
-        
+        ///SQL Constants
+        const string REF_TABLE = "Main_tbl";
+        const string JOB_FAIR_TABLE = "JobFair_Sring_16";
+        const string column_name = "id, name, major, grad_date, jobfair_year, time";
+      
+
+
         private class odbc_comand
         {
             OdbcCommand cmd;
@@ -30,7 +38,7 @@ namespace COEJOBFAIR_Alpha
 
             public odbc_cnn()
             {
-                cnn = new OdbcConnection("DSN=COEJOBFAIR;UID=ttu\ryhutchi;PWD = Frixon1!!;");  // get login from Remi that is not mine!
+                cnn = new OdbcConnection("DSN=COEJOBFAIR;UID=coe_ponos_admin;PWD = T~E95UeocGg_;");  
             }
 
             public OdbcConnection getcnn() { return cnn; }
@@ -56,10 +64,17 @@ namespace COEJOBFAIR_Alpha
                 return data_name_crd;
             }
 
+           //************* Database calls+++++++++++++++++++++
+
+                /// <summary>
+                /// First Main Db call get R-number and refrences it getting all data to put in array so that the printer can access the data.
+                /// This call gets the TABLE const which will have the correct database table to refrence
+                /// Rnum is given from main.cs call read from the usb scanner.
+                /// </summary>
             public void mainDBCall()
             {
                 odbc_cnn connection_1 = new odbc_cnn();
-                odbc_comand command_1 = new odbc_comand("SELECT * FROM Main_tbl WHERE id = " + Rnum);
+                odbc_comand command_1 = new odbc_comand("SELECT * FROM " + REF_TABLE + " WHERE id = " + Rnum);
 
                 command_1.getcmd().Connection = connection_1.getcnn();
                 connection_1.getcnn().Open();
@@ -72,10 +87,38 @@ namespace COEJOBFAIR_Alpha
                     data_name_crd[3] = reader[4].ToString();
                 }
 
+               
+                DBLOGCall(connection_1, Rnum);
+
                 reader.Close();
                 connection_1.getcnn().Close();
                  
             }
+
+
+            /// <summary>
+            /// This is the function that adds the student to a diffrent database that logs the time and what jobfair the student attended.
+            /// It will log all the students data need for data extraction because of lack of consistency in main reference database.
+            /// </summary>
+            /// <param name="connection">sends the connection that is already open so that this command can use it before it closes</param>
+            /// <param name="rnum">sends the Rnum for logging</param>
+            private void DBLOGCall(odbc_cnn connection, string rnum)
+            {
+                DateTime localDate = DateTime.Now;
+                                                                                                                                    //name first + last                            \\      //major            \\     //Graduation date \\   //jobfair\\  //date and time\\
+                odbc_comand command_2 = new odbc_comand("INSERT INTO " + JOB_FAIR_TABLE + "("+column_name+") VALUES ('"+ Rnum + "','"+ data_name_crd[0] + " " + data_name_crd[1] + "', '" + data_name_crd[2] + "', '"+data_name_crd[3] +"', '2016S', '" + localDate.Kind + "'");
+                
+                command_2.getcmd().Connection = connection.getcnn();
+                command_2.getcmd().ExecuteNonQuery();
+            }
+
+         
+
+            //************* Database calls*****************************************---------------
+
+
+
+            //************Connection Checks*******************************************************++++++++++++++
             public bool connection_chck()
             {
                 bool bol = true;
@@ -134,6 +177,7 @@ namespace COEJOBFAIR_Alpha
                 }
                 return val;
             }
+            //**************** Connection Check End*******************************************************----------------------------
                 
 
             }
