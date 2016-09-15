152c17e891b33e0e7895c2c56e60b5babc28c9ea
Ryan Hutchinson <ryanahutchinson@gmail.com>
2016-09-14 20:30:40 -0500

    Commit

--- COEJOBFAIR_Alpha/COEJOBFAIR_Alpha/Employer.Designer.cs
+++ COEJOBFAIR_Alpha/COEJOBFAIR_Alpha/Employer.Designer.cs
@@ -39,13 +39,14 @@ namespace COEJOBFAIR_Alpha
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.lst_hotels = new System.Windows.Forms.ListBox();
-            this.cmb_nights = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.txt_other = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // txt_name_first
@@ -98,7 +99,7 @@ namespace COEJOBFAIR_Alpha
             // chck_box_alumni
             // 
             this.chck_box_alumni.AutoSize = true;
-            this.chck_box_alumni.Location = new System.Drawing.Point(6, 19);
+            this.chck_box_alumni.Location = new System.Drawing.Point(351, 25);
             this.chck_box_alumni.Name = "chck_box_alumni";
             this.chck_box_alumni.Size = new System.Drawing.Size(165, 24);
             this.chck_box_alumni.TabIndex = 7;
@@ -107,6 +108,7 @@ namespace COEJOBFAIR_Alpha
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gray;
             this.groupBox1.Controls.Add(this.txt_name_first);
             this.groupBox1.Controls.Add(this.txt_name_last);
             this.groupBox1.Controls.Add(this.txt_organization);
@@ -121,11 +123,10 @@ namespace COEJOBFAIR_Alpha
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.lst_hotels);
-            this.groupBox2.Controls.Add(this.cmb_nights);
+            this.groupBox2.BackColor = System.Drawing.Color.Gray;
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.txt_other);
-            this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.chck_box_alumni);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(116, 220);
@@ -139,32 +140,15 @@ namespace COEJOBFAIR_Alpha
             // 
             this.lst_hotels.ForeColor = System.Drawing.SystemColors.WindowText;
             this.lst_hotels.ItemHeight = 20;
-            this.lst_hotels.Location = new System.Drawing.Point(369, 46);
+            this.lst_hotels.Location = new System.Drawing.Point(366, 19);
             this.lst_hotels.Name = "lst_hotels";
-            this.lst_hotels.Size = new System.Drawing.Size(244, 104);
+            this.lst_hotels.Size = new System.Drawing.Size(240, 84);
             this.lst_hotels.TabIndex = 0;
-            // 
-            // cmb_nights
-            // 
-            this.cmb_nights.FormattingEnabled = true;
-            this.cmb_nights.Items.AddRange(new object[] {
-            "",
-            "0",
-            "6 or more",
-            "5",
-            "4",
-            "3",
-            "2",
-            "1"});
-            this.cmb_nights.Location = new System.Drawing.Point(370, 161);
-            this.cmb_nights.Name = "cmb_nights";
-            this.cmb_nights.Size = new System.Drawing.Size(243, 28);
-            this.cmb_nights.TabIndex = 12;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 164);
+            this.label2.Location = new System.Drawing.Point(36, 186);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(309, 20);
             this.label2.TabIndex = 11;
@@ -172,7 +156,7 @@ namespace COEJOBFAIR_Alpha
             // 
             // txt_other
             // 
-            this.txt_other.Location = new System.Drawing.Point(619, 46);
+            this.txt_other.Location = new System.Drawing.Point(612, 77);
             this.txt_other.Name = "txt_other";
             this.txt_other.Size = new System.Drawing.Size(202, 26);
             this.txt_other.TabIndex = 10;
@@ -182,21 +166,29 @@ namespace COEJOBFAIR_Alpha
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 46);
+            this.label1.Location = new System.Drawing.Point(6, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(357, 20);
             this.label1.TabIndex = 8;
             this.label1.Text = "What is the name of the Hotel you are staying at?";
             // 
-            // label3
+            // textBox1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(426, 9);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(168, 25);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Hello Employers";
+            this.textBox1.Location = new System.Drawing.Point(372, 186);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(67, 26);
+            this.textBox1.TabIndex = 12;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.lst_hotels);
+            this.groupBox3.Controls.Add(this.txt_other);
+            this.groupBox3.Location = new System.Drawing.Point(6, 49);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(865, 122);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
             // 
             // Employer
             // 
@@ -204,7 +196,6 @@ namespace COEJOBFAIR_Alpha
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Firebrick;
             this.ClientSize = new System.Drawing.Size(1105, 572);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.submit);
@@ -214,8 +205,9 @@ namespace COEJOBFAIR_Alpha
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -236,9 +228,9 @@ namespace COEJOBFAIR_Alpha
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txt_other;
-        private System.Windows.Forms.ComboBox cmb_nights;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ListBox lst_hotels;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
